f9e27380880f905e43bfcdcf87d1b582e3b4322a
Victoria Kumar <vital@MacBook-Air.local>
2025-06-07 19:52:18 +0200

    Fix: remove direct call to generate_daily_advice

--- manage.py
+++ manage.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
-from home.views import generate_daily_advice
-generate_daily_advice()
+#from home.views import generate_daily_advice
+#generate_daily_advice()
